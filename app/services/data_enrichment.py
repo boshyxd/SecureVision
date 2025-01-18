4c4e5644d0c570238922ed3d6f7ad34784229c45
James Liang <86000097+jamesliangg@users.noreply.github.com>
2025-01-18 13:33:59 -0500

    Add captcha checker

--- app/services/data_enrichment.py
+++ app/services/data_enrichment.py
@@ -1,11 +1,12 @@
 import aiohttp
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
-from app.models.database import SessionLocal, BreachEntry
+# from app.models.database import SessionLocal, BreachEntry
 import shodan
 import os
 import dotenv
 import requests
+import re
 
 dotenv.load_dotenv()
 SHODAN_API_KEY = os.getenv("SHODAN_API_KEY")
@@ -70,3 +71,23 @@ def shodan_resolve_dns(hostnames):
     except requests.exceptions.RequestException as e:
         print(f"Error resolving DNS: {e}")
         return None
+
+
+def check_captcha(url):
+    response = requests.get(url)
+    html_content = response.text
+    captcha_patterns = [
+        r"google\.com/recaptcha",  # reCAPTCHA
+        r"gstatic\.com/recaptcha",  # reCAPTCHA assets
+        r"hcaptcha\.com",  # hCaptcha
+        r"\.hcaptcha\.com/1/api.js",  # hCaptcha script
+        r"data-sitekey",  # Common attribute for CAPTCHA widgets
+        r"captcha",  # General keyword
+    ]
+
+    # Search for patterns
+    if any(re.search(pattern, html_content, re.IGNORECASE) for pattern in captcha_patterns):
+        return True
+    else:
+        return False
+
